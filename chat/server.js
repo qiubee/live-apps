ef44414db9ff0598f73e5ce2b90921e3e22d1f8b
qiubee <qiubees@hotmail.com>
2020-04-06 16:41:15 +0200

    add socket.io module

--- chat/server.js
+++ chat/server.js
@@ -1,17 +1,25 @@
 const express = require("express");
 const router = require("./routes/router");
-const io = require("socket.io");
 
 // server
 const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
 const port = 8000;
 
 // use public folder for static files
-app.use(express.static("public"));
+app.use(express.static("chat/public"));
 
 // routing
 app.use("/", router);
 
-app.listen(port, function () {
+io.on("connection", function(socket) {
+    console.log("a user connected");
+    socket.on("disconnect", function() {
+        console.log("user disconnected");
+    });
+});
+
+server.listen(port, function () {
     console.log(`Listening on port \u001b[1m\u001b[36m${port}\u001b[0m\n\u001b[1m\u001b[36mlocalhost:${port}\u001b[0m`);
 });
