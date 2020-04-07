f59cd2e74ed46108c3822a062ad25e2deb639390
qiubee <qiubees@hotmail.com>
2020-04-07 12:57:36 +0200

    add IIFE to send message to server via socket

--- chat/public/js/index.js
+++ chat/public/js/index.js
@@ -1,1 +1,11 @@
-const socket = io();
+(function() {
+    const socket = io();
+    const form = document.querySelector("form");
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+        const input = document.getElementById("m").value;
+        socket.emit("chat message", input);
+        input.value = "";
+        return false;
+    });
+})();
